ef023480362e3386903da1ac64a29e1f8e6cebf9
Chaghall <simon.grevet.17@eigsi.fr>
2014-02-08 22:49:37 +0100

    Remplissage d'une grille

--- src/CreationGrille.java
+++ src/CreationGrille.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 
 public class CreationGrille {
-	
+
 	public static void main(String[] args){
 		new CreationGrille();
 	}
@@ -14,21 +14,34 @@ public class CreationGrille {
 	{
 		int x = 5, y = 5;
 		long seed = 42;
-		Random List = new Random(seed);
-		//		boolean tab[][] = new boolean[x][y];       // nouveau tableau pour désigné cases morts et vivant
-		//		int tab2[][] = {{1,1},{1,2},{2,3},{3,4}};
+		Random List = new Random(seed);		// Seed du tableau
+		//boolean tab[][] = new boolean[x][y];		// Tableau de "forme" de grille
 		boolean tab[][] = {{true, true, false, true, true},{true, true, true, true, true},{false, true, true, true, false},{true, true, true, true, true},{true, true, false, true, true}};
-		int grill[][] = new int[x][y];
+		int grill[][] = new int[x][y];		// Tableau de valeur sans bordures
 
-		for(int i=0;i<tab.length; i++){
-			for(int j = 0 ; j< tab[i].length; j++){
-				if (tab[i][j])
-					grill[i][j] = List.nextInt(9)+1;
-			}
-		}
-		
-		System.out.println(tab.length);
-		
+
+		for(int i=0;i<tab.length; i++)		// Remplissage des valeurs de la grille
+			for(int j = 0 ; j< tab[i].length; j++)
+				if (tab[i][j]){
+					int nb = List.nextInt(9) + 1;
+					boolean test = false;
+					while(!test){
+						boolean testI = true, testJ = true;
+						for(int ii=i-1; ii>=0 && grill[ii][j]!=0; ii--)		// Test de la présence ou non du chiffre actuel sur la ligne
+							if(grill[ii][j]==nb)
+								testI = false;
+						for(int jj=j-1; jj>=0 && grill[i][jj]!=0; jj--)		// Test de la présence ou non du chiffre actuel sur la colonne
+							if(grill[i][jj]==nb)
+								testJ = false;
+						if (testI && testJ){
+							test = true;		// Si les tests ont réussi, on place le chiffre actuel dans la case en cours
+							grill[i][j] = nb;		// On remplit de 1 à 9
+						}
+						else
+							nb = List.nextInt(9) + 1;	// Autrement, on change de valeur
+					}
+				}
+
 		AffichTab(grill);
 
 
@@ -68,6 +81,5 @@ public class CreationGrille {
 				System.out.print("|"+tab[i][j]);
 			System.out.println("|");
 		}
-		System.out.println();
 	}
 }
