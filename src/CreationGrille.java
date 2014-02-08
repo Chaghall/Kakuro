bb6704fd2c31d9b84819a80079f3bb07c05e93a2
Chaghall <simon.grevet.17@eigsi.fr>
2014-02-08 14:35:16 +0100

    Grille fixe

--- src/CreationGrille.java
+++ src/CreationGrille.java
@@ -4,16 +4,38 @@ import java.util.Random;
 
 
 public class CreationGrille {
-
 	
-	public void Creationgrille()
+	public static void main(String[] args){
+		new CreationGrille();
+	}
+
+
+	public CreationGrille()
 	{
-		int x = 0, y = 4;
-		Random List = new Random();
-		boolean tab[][] = new boolean[x][y];       // nouveau tableau pour désigné mrots et vivant
+		int x = 5, y = 5;
+		long seed = 42;
+		Random List = new Random(seed);
+		//		boolean tab[][] = new boolean[x][y];       // nouveau tableau pour désigné cases morts et vivant
+		//		int tab2[][] = {{1,1},{1,2},{2,3},{3,4}};
+		boolean tab[][] = {{true, true, false, true, true},{true, true, true, true, true},{false, true, true, true, false},{true, true, true, true, true},{true, true, false, true, true}};
+		int grill[][] = new int[x][y];
+
+		for(int i=0;i<tab.length; i++){
+			for(int j = 0 ; j< tab[i].length; j++){
+				if (tab[i][j])
+					grill[i][j] = List.nextInt(9)+1;
+			}
+		}
 		
+		System.out.println(tab.length);
+		
+		AffichTab(grill);
+
+
+		/*
 		List<Nombre> grille = new ArrayList<Nombre>();
-		
+
+
 		for(int cpt1 = 1; cpt1<y ; cpt1++)// initialisé à 1 car deux bords doivent êtres "morts"
 		{
 			for(int cpt2 =1; cpt2<x ; cpt2++)
@@ -31,9 +53,21 @@ public class CreationGrille {
 					for(int gx = cpt2; gx < cpt2 + List.nextInt(7); gx++) // 
 						{
 						grille.add(new Nombre(List.nextInt(10)));
+
+
 					}
 				}
 			}
 		}
+		 */	
+	}	
+
+	private void AffichTab(int tab[][]){
+		for (int i=0;i<tab.length; i++){
+			for (int j=0; j<tab[i].length; j++)
+				System.out.print("|"+tab[i][j]);
+			System.out.println("|");
+		}
+		System.out.println();
 	}
 }
--- src/Nombre.java
+++ src/Nombre.java
@@ -1,8 +1,14 @@
 
 public class Nombre {
-
+	
+	/**
+	 * Genère un tableaux
+	 * @param l
+	 */
 	public Nombre(int l)
 	{
+		int[] combi;
 		
 	}
+	
 }
