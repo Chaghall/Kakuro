6f3ce8476698cd1e8b54fa6304aa166ff0691861
Chaghall <simon.grevet.17@eigsi.fr>
2014-02-08 22:57:46 +0100

    Nettoyage

--- src/CreationGrille.java
+++ src/CreationGrille.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 
@@ -43,36 +41,6 @@ public class CreationGrille {
 				}
 
 		AffichTab(grill);
-
-
-		/*
-		List<Nombre> grille = new ArrayList<Nombre>();
-
-
-		for(int cpt1 = 1; cpt1<y ; cpt1++)// initialisé à 1 car deux bords doivent êtres "morts"
-		{
-			for(int cpt2 =1; cpt2<x ; cpt2++)
-			{
-				tab[cpt1][cpt2] =List.nextBoolean(); // 0 = mort ; 1 = vivante
-			}
-		}
-		for(int cpt1 = 1; cpt1<x ; cpt1++)
-		{
-			for(int cpt2 =1; cpt2<x ; cpt2++)
-			{
-				if(tab[cpt1][cpt2]) // par defaut : TRUE
-				{
-				for(int gy =cpt1; gy < cpt1 + List.nextInt(7); gy++)
-					for(int gx = cpt2; gx < cpt2 + List.nextInt(7); gx++) // 
-						{
-						grille.add(new Nombre(List.nextInt(10)));
-
-
-					}
-				}
-			}
-		}
-		 */	
 	}	
 
 	private void AffichTab(int tab[][]){
