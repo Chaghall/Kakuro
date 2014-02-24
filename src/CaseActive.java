fa8f57752ad096157da82a9ac3869053427a896f
Chaghall <simon.grevet.17@eigsi.fr>
2014-02-24 13:36:36 +0100

    Test

--- src/CaseActive.java
+++ src/CaseActive.java
@@ -1,9 +1,43 @@
-
 public class CaseActive {
 
+	private int n;
+	private int sX;
+	private int sY;
 	
-	private void CaseActive()
+	/**
+	 * 
+	 * @param n valeur d'une case
+	 */
+	public CaseActive(int n)
 	{
-		
+		this.n = n;
 	}
+	
+	/**
+	 * 
+	 * @param S1 indice de Somme
+	 * @param or1 orientation de l'indice de Somme
+	 */
+	public CaseActive(int S, boolean or1)
+	{
+		if(or1)
+			this.sX = S;
+		else
+			this.sY = S;
+			
+	}
+
+	/**
+	 * 
+	 * @param S1 indice de Somme vertical
+	 * @param S2 indice de Somme horizontal
+	 */
+	public CaseActive(int S1, int S2)
+	{
+		this.sX = S1;
+		this.sY = S2;
+	}
+	
+	
+
 }
--- src/CreationGrille.java
+++ src/CreationGrille.java
@@ -1,11 +1,11 @@
 import java.util.Random;
 
-@SuppressWarnings("unused")
+
 public class CreationGrille {
 
 	public static void main(String[] args){
 		new CreationGrille();
-		
+
 	}
 
 
@@ -17,7 +17,14 @@ public class CreationGrille {
 		boolean[][] forme = formeGrille(x, y, List);
 		int[][] valeur = remplissageGrille(x, y, List, forme);
 		affichTab(valeur);
-
+		
+		
+		
+		for (int i = 0; i < valeur.length; i++)
+			for (int j = 0; i < valeur[i].length; i++)
+			{
+				
+			}
 	}
 
 
@@ -38,7 +45,7 @@ public class CreationGrille {
 			int val = List.nextInt((x-1)*(y-1));
 			int j = val % (y-1);
 			int i = val / (x-1);
-			
+
 			/*
 			 * On va maintenant poser un bloc de 2x2 sur la coordonnée en question
 			 */
@@ -82,79 +89,11 @@ public class CreationGrille {
 							nb = List.nextInt(9) + 1;	// Autrement, on change de valeur
 					}
 				}
-		affichTab(grill);
-		CopieTab(grill);
 		return grill;
-		
-<<<<<<< HEAD
-=======
 	}	
 
-	/**
-	 * Génération d'un tableau de forme de grille
-	 * @param x
-	 * @param y
-	 * @param seed
-	 * @return
-	 */
-	private boolean[][] FormeGrille(int x, int y, long seed) {
 
-		boolean frmGrll[][] = new boolean[x][y];
-		Random List = new Random(seed);
-
-		for(int i = 0; i < frmGrll.length ; i++)
-		{			//Création du tableau de booléens
-			for(int j = 0; j < frmGrll[i].length ; j++)
-			{
-				frmGrll[i][j] = List.nextBoolean();			// Aléatoire, bien sûr
-			}
-		}
-
-		//affichTab(frmGrll);
-
-		boolean frmGrllTest[][] = new boolean[x+2][y+2];
-
-		for(int i = 0; i < frmGrll.length ; i++){			// Création d'un second tableau sur lequel on effectue les changements pour obtenir les suites de nombres > 2.
-			for(int j = 0; j < frmGrll[i].length ; j++){
-				frmGrllTest[i+1][j+1] = frmGrll[i][j];
-			}
-		}
-		if(frmGrllTest[1][1]){		// Optimisation possible, C'est pour ne pas avoir de changements si la première case est vraie
-			frmGrllTest[0][1] = true;
-			frmGrllTest[1][0] = true;
-		}
-
-
-		for(int i = 1; i < frmGrllTest.length-1 ; i++){		// Création des listes de longueur de sommes
-			for(int j = 1; j < frmGrllTest[i].length-1 ; j++){
-				int n = 0;
-				if(frmGrllTest[i-1][j])
-					n += 1;
-				if(frmGrllTest[i+1][j])
-					n += 1;
-				if(frmGrllTest[i][j-1])
-					n += 1;
-				if(frmGrllTest[i][j+1])
-					n += 1;
-				if (n>1)
-					frmGrllTest[i][j] = true;
-				else
-					frmGrllTest[i][j] = false;
-			}
-		}
-		//affichTab(frmGrllTest);
-
-		for(int i = 0; i < frmGrll.length ; i++){			// Et on remet dans le premier tableau
-			for(int j = 0; j < frmGrll[i].length ; j++){
-				frmGrll[i][j] = frmGrllTest[i+1][j+1];
-			}
-		}
-		//affichTab(frmGrll);
-		return frmGrll;
->>>>>>> dd32b6bfe8838b6fe730b298a71ab6edc4645db0
-	}
-	
-
+	@SuppressWarnings("unused")
 	private void affichTab(int tab[][]){
 		for (int i=0;i<tab.length; i++){
 			for (int j=0; j<tab[i].length; j++)
@@ -162,11 +101,9 @@ public class CreationGrille {
 			System.out.println("|");
 		}
 	}
-<<<<<<< HEAD
-	
-	
-=======
->>>>>>> dd32b6bfe8838b6fe730b298a71ab6edc4645db0
+
+
+	@SuppressWarnings("unused")
 	private void affichTab(boolean tab[][])
 	{
 		for (int i=0;i<tab.length; i++){
@@ -175,46 +112,37 @@ public class CreationGrille {
 			System.out.println("|");
 		}
 	}
+
+
+
 	private void CalcSommeY(int tabSommes[][])
 	{
 		int somme = 0;
 		for(int i =0; i<tabSommes.length; i++)   // on parcours le tableau
-		{
 			for(int j =0; j<tabSommes.length; j++)
-			{
-				if(tabSommes[i][j]==0)            //si on trouve un 0 
+				if(tabSommes[i][j]==0)            //si on trouve un 0
 				{
-					for(int j2 = j+1; tabSommes[i][j] == 0 || j == tabSommes.length; j2++ )// on effectue tant que on ne rtombe pas sur un 0 ou que l'on est à la fin du tableau
-					{
-						 somme = tabSommes[i][j2];
-					}
+					//On effectue tant que on ne retombe pas sur un 0 ou que l'on est à la fin du tableau
+					for(int j2 = j+1; tabSommes[i][j] != 0 && j != tabSommes.length; j2++ )
+						somme = tabSommes[i][j2];
 					tabSommes[i][j] = somme;
-					
 				}
-			}
-		}
 	}
-	
+
 	private void CalcSommeX(int tabSommes[][])
 	{
 		int somme = 0;
 		for(int i =0; i<tabSommes.length; i++)   // on parcours le tableau
-		{
 			for(int j =0; j<tabSommes.length; j++)
-			{
 				if(tabSommes[i][j]==0)            //si on trouve un 0 
 				{
-					for(int i2 = i+1; tabSommes[i][j] == 0 || i == tabSommes.length; i2++ )// on effectue tant que on ne rtombe pas sur un 0 ou que l'on est à la fin du tableau
-					{
-						 somme = tabSommes[i2][j];
-					}
+					//On effectue tant que on ne retombe pas sur un 0 ou que l'on est à la fin du tableau
+					for(int i2 = i+1; tabSommes[i][j] != 0 && i != tabSommes.length; i2++ )
+						somme = tabSommes[i2][j];
 					tabSommes[i][j] = somme;
-					
 				}
-			}
-		}
 	}
-	
+
 	private void CopieTab(int grill[][])
 	{
 		int x=0;
@@ -223,12 +151,10 @@ public class CreationGrille {
 		for(int i = 0; i<tabSommes.length; i++)
 		{
 			for(int j=0; j<tabSommes[i].length; j++ )
-			{
 				tabSommes[i][j] = grill[i][j];
-			}
 		}
 		affichTab(tabSommes);
 	}
-	
+
 }
 
